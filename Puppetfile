7a1c13a753ed8931e775664593ca15cc9f76269c
Jeffrey Clark <h0tw1r3@users.noreply.github.com>
2022-04-01 11:02:53 -0500

    initial commit

---
+++ Puppetfile
@@ -0,0 +1,11 @@
+# This Puppetfile is managed by Bolt. Do not edit.
+# For more information, see https://pup.pt/bolt-modules
+
+# The following directive installs modules to the managed moduledir.
+moduledir '.modules'
+
+mod 'puppetlabs/apt', '8.3.0'
+mod 'puppet/archive', '6.0.2'
+mod 'puppet/unattended_upgrades', '6.0.0'
+mod 'puppetlabs/stdlib', '8.1.0'
+mod 'puppetlabs/java', '7.3.0'
---
+++ plans/deploy.pp
@@ -0,0 +1,80 @@
+# Deploy Omada SDN Controller
+plan omada_sdn::deploy (
+  TargetSpec $targets
+) {
+  $terraform_result = run_task(
+    'terraform::apply',
+    'localhost',
+    dir => './terraform'
+  )
+
+  apply_prep($targets)
+
+  $apply_result = apply($targets, _catch_errors => true, _run_as => root) {
+    class { 'unattended_upgrades':
+      auto                   => {
+        reboot => true,
+        clean  => 7,
+        remove => true,
+      },
+      extra_origins          => [
+        '${distro_id}:${distro_codename}-updates',
+      ],
+      remove_new_unused_deps => true,
+      syslog_enable          => true,
+      days                   => ["0", "1", "2", "3", "4", "5", "6"],
+    }
+    -> file { '/etc/rsyslog.d/listen.conf':
+      ensure  => present,
+      notify  => Exec['restart-rsyslog'],
+      content => @(EOD)
+        module(load="imudp")
+        input(type="imudp" port="514")
+        module(load="imtcp")
+        input(type="imtcp" port="514")
+        | EOD
+    }
+    -> class { 'java': }
+    -> package { 'jsvc': }
+    -> file { '/usr/lib/jvm/java-11-openjdk-amd64/lib/amd64/':
+      ensure => directory,
+    }
+    -> file { '/usr/lib/jvm/java-11-openjdk-amd64/lib/amd64/server':
+      ensure => link,
+      target => '../server',
+    }
+    file { '/usr/share/keyrings/mongodb-44-keyring.asc':
+      ensure => present,
+      source => 'https://www.mongodb.org/static/pgp/server-4.4.asc',
+      notify => Exec['mongodb-apt-update'],
+    }
+    -> file { '/etc/apt/sources.list.d/mongodb.list':
+      content => @(EOD)
+        # Managed by Puppet
+        deb [signed-by=/usr/share/keyrings/mongodb-44-keyring.asc arch=amd64,arm64] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse 
+        | EOD
+    }
+    ~> exec { 'mongodb-apt-update':
+      command     => '/usr/bin/env apt-get -d update',
+      refreshonly => true,
+    }
+    -> package { 'mongodb-org': }
+    -> package { 'curl': }
+    -> file { '/var/tmp/omada_sdn_controller_v5.deb':
+      ensure         => present,
+      source         => 'https://static.tp-link.com/upload/software/2022/202201/20220120/Omada_SDN_Controller_v5.0.30_linux_x64.deb',
+      checksum_value => '92abc1274d580e631c71b60e79afd8b08be65ca5387c625eea1a040cbaee0ccf',
+    }
+    -> package { 'omadac':
+      provider => dpkg,
+      ensure   => installed,
+      source   => '/var/tmp/omada_sdn_controller_v5.deb',
+    }
+
+    exec { 'restart-rsyslog':
+      command     => '/usr/bin/env systemctl restart rsyslog',
+      refreshonly => true,
+    }
+  }
+  return $apply_result
+}
